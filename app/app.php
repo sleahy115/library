d1de2882be4460ee8aa1accabc94a264d104c1ad
Andrew Devlin and Sarah Leahy <andrew@>
2017-03-01 11:52:47 -0800

    create twig templates for index and book list

--- app/app.php
+++ app/app.php
@@ -2,14 +2,14 @@
 date_default_timezone_set('America/Los_Angeles');
 
 require_once __DIR__ . "/../vendor/autoload.php";
-require_once __DIR__ . "/../src/Student.php";
-require_once __DIR__ . "/../src/Course.php";
+require_once __DIR__ . "/../src/Patron.php";
+require_once __DIR__ . "/../src/Book.php";
 
 $app = new Silex\Application();
 
 $app['debug'] = true;
 
-$server = 'mysql:host=localhost:8889;dbname=school';
+$server = 'mysql:host=localhost:8889;dbname=library';
 $username = 'root';
 $password = 'root';
 $DB = new PDO($server, $username, $password);
@@ -22,80 +22,21 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 ));
 
 $app->get("/", function() use ($app) {
-    return $app['twig']->render('homepage.html.twig');
+    return $app['twig']->render('index.html.twig');
 });
 
-$app->get("/students", function() use ($app) {
-    return $app['twig']->render('students.html.twig', array('students' => Student::getAll()));
+$app->get("/book_list", function() use ($app) {
+    if (Book::getAll() == false){
+        $books= array();
+        var_dump($books);
+    }else {
+        $books = Book::getAll();
+    }
+    return $app['twig']->render('book_list.html.twig', array('books'=>$books));
 });
 
-$app->get("/update_student/{id}", function($id) use ($app) {
-    $student = Student::find($id);
-    return $app['twig']->render('edit_student.html.twig', array('student' => $student));
-});
-$app->get("/update_course/{id}", function($id) use ($app) {
-    $course = Course::find($id);
-    return $app['twig']->render('edit_course.html.twig', array('course' => $course, 'students_in_class' => $course->getStudents(), 'all_students' => Student::getAll()));
-});
-$app->get("/courses", function() use ($app) {
-    return $app['twig']->render('courses.html.twig', array('courses' => Course::getAll()));
-});
-
-$app->post("/add_class", function() use ($app) {
-    $id = null;
-    $name = $_POST['name'];
-    $course_number = $_POST['course_number'];
-    $new_course = new Course($id, $name, $course_number);
-    $new_course->save();
-    return $app['twig']->render('courses.html.twig', array('courses' => Course::getAll()));
-});
-$app->post("/add_student", function() use ($app) {
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $enrolment_date = $_POST['enrolment_date'];
-    $new_student = new Student(null, $first_name, $last_name, $enrolment_date);
-    $new_student->save();
-    return $app['twig']->render('students.html.twig', array('students' => Student::getAll()));
-});
-$app->delete("/delete_student/{id}", function($id) use ($app) {
-    $student = Student::find($id);
-    $student->delete();
-    return $app['twig']->render('students.html.twig', array('students' => Student::getAll()));
-});
-$app->delete("/delete_course/{id}", function($id) use ($app) {
-    $course = Course::find($id);
-    $course->delete();
-    return $app['twig']->render('courses.html.twig', array('courses' => Course::getAll()));
-});
-$app->delete("/delete_all_students", function() use ($app) {
-    Student::deleteAll();
-    return $app['twig']->render('students.html.twig', array('students' => Student::getAll()));
-});
-
-
-$app->patch("/student_editor/{id}", function($id) use ($app) {
-    $student = Student::find($id);
-    $student->updateFirstName($_POST['first_name']);
-    $student->updateLastName($_POST['last_name']);
-    $student->updateEnrolmentDate($_POST['enrolment_date']);
-    return $app['twig']->render('students.html.twig', array('students' => Student::getAll()));
-});
-$app->patch("/course_editor/{id}", function($id) use ($app) {
-    $course = Course::find($id);
-    $course->updateName($_POST['name']);
-    $course->updateCourseNumber($_POST['course_number']);
-    return $app['twig']->render('courses.html.twig', array('courses' => Course::getAll()));
-});
-
-$app->post("/add_student_to_course/{id}", function($id) use ($app) {
-    $course = Course::find($id);
-    $student_id = $_POST['students'];
-    $student = Student::find($student_id);
-    $course->addStudent($student);
-
-    return $app['twig']->render('edit_course.html.twig', array('course' => $course, 'students_in_class' => $course->getStudents(), 'all_students' => Student::getAll()));
-});
-
-
+//
+// $new_book = new Book($_POST['title'],$_POST['due_date'], $_POST['checkout_date'], $_POST['author']);
+// $new_book->save();
 return $app;
 ?>
