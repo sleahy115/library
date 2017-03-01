2d8dc4d797ab8a66ce05c0dba5c7d395e53ac1c7
Andrew Devlin and Sarah Leahy <sarah@>
2017-03-01 13:25:57 -0800

    create patron twig

--- app/app.php
+++ app/app.php
@@ -26,17 +26,45 @@ $app->get("/", function() use ($app) {
 });
 
 $app->get("/book_list", function() use ($app) {
-    if (Book::getAll() == false){
-        $books= array();
-        var_dump($books);
-    }else {
-        $books = Book::getAll();
-    }
-    return $app['twig']->render('book_list.html.twig', array('books'=>$books));
+    // if (Book::getAll() == false){
+    //     $books= array();
+    // }else {
+    //     $books = Book::getAll();
+    // }
+    return $app['twig']->render('book_list.html.twig', array('books'=>Book::getAll()));
+});
+$app->post("/book_list", function() use ($app) {
+    $new_book = new Book($_POST['title'],$_POST['due_date'], $_POST['checkout_date'], $_POST['author']);
+    $new_book->save();
+    return $app['twig']->render('book_list.html.twig', array('books'=>Book::getAll()));
+});
+$app->get("/delete", function() use ($app) {
+    Book::deleteAll();
+    return $app['twig']->render('index.html.twig', array('books'=>Book::getAll()));
+});
+$app->get("/book/{id}", function($id) use ($app) {
+    $book =Book::find($id);
+    return $app['twig']->render('book_details.html.twig', array('book'=>$book, 'patrons' => Patron::getAll()));
+});
+$app->get("/patron_list", function() use ($app) {
+    return $app['twig']->render('patron_list.html.twig', array('patrons'=>Patron::getAll()));
+});
+$app->post("/patron_list", function() use ($app) {
+    $new_patron = new Patron($_POST['name']);
+    $new_patron->save();
+    return $app['twig']->render('patron_list.html.twig', array('patrons'=>Patron::getAll()));
+});
+$app->get("/delete/patron", function() use ($app) {
+    Patron::deleteAll();
+    return $app['twig']->render('index.html.twig', array('patrons'=>Patron::getAll()));
+});
+$app->get("/patron/{id}", function($id) use ($app) {
+    $patron =Patron::find($id);
+    return $app['twig']->render('patron_details.html.twig', array('patron'=>$patron, 'patrons' => Patron::getAll()));
 });
 
-//
-// $new_book = new Book($_POST['title'],$_POST['due_date'], $_POST['checkout_date'], $_POST['author']);
-// $new_book->save();
+
+
+
 return $app;
 ?>
